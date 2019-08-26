fb8e17aec656e235f5da8c4e603a0a26266a81f0
adrianwit@gmail.com <Ubc!1234F>
2019-08-26 11:26:54 -0700

    added dir mode

--- file/create.go
+++ file/create.go
@@ -11,6 +11,7 @@ import (
 func Create(ctx context.Context, URL string, mode os.FileMode, isDir bool, options ...storage.Option) error {
 	filePath := Path(URL)
 	if isDir {
+		mode = mode | os.ModeDir
 		if err := EnsureParentPathExists(filePath, mode); err != nil {
 			return err
 		}
