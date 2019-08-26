b66a084b6a327e48f6911a38a8659355e9f54fd3
adrianwit@gmail.com <Ubc!1234F>
2019-08-26 10:08:12 -0700

    added basic content replacer

--- option/replacer.go
+++ option/replacer.go
@@ -1,1 +1,26 @@
 package option
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+//Replace return modification handler with the specified replacements map
+func Replace(replacements map[string]string) func(info os.FileInfo, reader io.ReadCloser) (io.ReadCloser, error) {
+	return func(info os.FileInfo, reader io.ReadCloser) (io.ReadCloser, error) {
+		data, err := ioutil.ReadAll(reader)
+		if err != nil {
+			return nil, err
+		}
+		_ = reader.Close()
+		text := string(data)
+		for k, v := range replacements {
+			if count := strings.Count(text, k); count > 0 {
+				text = strings.Replace(text, k, v, count)
+			}
+		}
+		return ioutil.NopCloser(strings.NewReader(text)), nil
+	}
+}
