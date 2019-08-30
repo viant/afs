7eaff63f145985b9d14b80aff5f53fe9d0b5abe7
adrianwit@gmail.com <Ubc!1234F>
2019-08-30 15:15:46 -0700

    added modification time matcher

---
+++ matcher/modification.go
@@ -0,0 +1,43 @@
+package matcher
+
+import (
+	"github.com/viant/afs/option"
+	"os"
+	"time"
+)
+
+//Modification represents modification matcher
+type Modification struct {
+	After    *time.Time
+	Before   *time.Time
+	matchers []option.Matcher
+}
+
+//Match matcher parent and info with matcher rules
+func (r *Modification) Match(parent string, info os.FileInfo) bool {
+	if r.After != nil {
+		if !r.After.Before(info.ModTime()) {
+			return false
+		}
+	}
+	if r.Before != nil {
+		if !r.Before.After(info.ModTime()) {
+			return false
+		}
+	}
+	for i := range r.matchers {
+		if !r.matchers[i](parent, info) {
+			return false
+		}
+	}
+	return true
+}
+
+//NewModification creates a modification time matcher
+func NewModification(before, after *time.Time, matchers ...option.Matcher) *Modification {
+	return &Modification{
+		Before:   before,
+		After:    after,
+		matchers: matchers,
+	}
+}
---
+++ matcher/modification_test.go
@@ -0,0 +1,111 @@
+package matcher
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/viant/afs/file"
+	"path"
+	"testing"
+	"time"
+)
+
+func timePtr(time time.Time) *time.Time {
+	return &time
+}
+
+func TestModification_Match(t *testing.T) {
+
+	var useCases = []struct {
+		description string
+		prefix      string
+		suffix      string
+		filter      string
+		before      *time.Time
+		after       *time.Time
+		modTime     *time.Time
+		location    string
+		expect      bool
+	}{
+		{
+			description: "prefix match",
+			prefix:      "foo/",
+			location:    "foo/abc.txt",
+			expect:      true,
+		},
+		{
+			description: "prefix no match",
+			prefix:      "zfoo/",
+			location:    "foo/abc.txt",
+			expect:      false,
+		},
+		{
+			description: "suffix match",
+			suffix:      "txt",
+			location:    "foo/abc.txt",
+			expect:      true,
+		},
+		{
+			description: "suffix no match",
+			suffix:      ".abc",
+			location:    "foo/abc.txt",
+			expect:      false,
+		},
+
+		{
+			description: "regexpr match",
+			filter:      "asset\\d+\\.txt",
+			location:    "asset0001.txt",
+			expect:      true,
+		},
+		{
+			description: "regexpr no match",
+			filter:      "asset\\d+\\.txt",
+			location:    "asset.txt",
+			expect:      false,
+		},
+		{
+			description: "before time match",
+			modTime:     timePtr(time.Now()),
+			before:      timePtr(time.Now().Add(time.Hour)),
+			location:    "asset.txt",
+			expect:      true,
+		},
+		{
+			description: "before no time match",
+			modTime:     timePtr(time.Now()),
+			before:      timePtr(time.Now().Add(-time.Hour)),
+			location:    "asset.txt",
+			expect:      false,
+		},
+		{
+			description: "after time match",
+			modTime:     timePtr(time.Now()),
+			after:       timePtr(time.Now().Add(time.Hour)),
+			location:    "asset.txt",
+			expect:      false,
+		},
+		{
+			description: "after no time match",
+			modTime:     timePtr(time.Now()),
+			after:       timePtr(time.Now().Add(-time.Hour)),
+			location:    "asset.txt",
+			expect:      true,
+		},
+	}
+
+	for _, useCase := range useCases {
+		basic, err := NewBasic(useCase.prefix, useCase.suffix, useCase.filter)
+		if !assert.Nil(t, err, useCase.description) {
+			continue
+		}
+		matcher := NewModification(useCase.before, useCase.after, basic.Match)
+		parent, name := path.Split(useCase.location)
+		modTime := time.Now()
+		if useCase.modTime != nil {
+			modTime = *useCase.modTime
+		}
+		info := file.NewInfo(name, 0, 0644, modTime, false)
+		actual := matcher.Match(parent, info)
+		assert.EqualValues(t, useCase.expect, actual, useCase.description)
+	}
+
+}
