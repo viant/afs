76fe5c8b9988e176615c788e4e5da250b8306d17
adrianwit <adrianwit@gmail.com>
2020-02-07 12:45:46 -0800

    updated meta constats

--- option/content/const.go
+++ option/content/const.go
@@ -3,4 +3,8 @@ package content
 const (
 	//Type represents content meta type
 	Type = "Content-Type"
+	//Encoding content encoding
+	Encoding="Content-Encoding"
+	//Language content languate
+	Language = "Content-Language"
 )
