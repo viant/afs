dc23d64dda15718156eba65976e6302af8f06033
awitas <awitas@vindicotech.com>
2022-06-01 14:09:02 -0700

    updated normalize URL

--- url/normalize.go
+++ url/normalize.go
@@ -1,7 +1,19 @@
 package url
 
+import (
+	"os"
+	"strings"
+)
+
 //Normalize normalizes URL
 func Normalize(URL, scheme string) string {
+	if strings.Index(URL, ":") == -1 {
+		if URL != "" && URL[0] != '/' {
+			if basePath, err := os.Getwd(); err == nil {
+				URL = Join(basePath, URL)
+			}
+		}
+	}
 	schema := Scheme(URL, scheme)
 	baseURL, URLPath := Base(URL, schema)
 	return Join(baseURL, URLPath)
--- url/path.go
+++ url/path.go
@@ -15,7 +15,6 @@ func Path(URL string) string {
 	if runtime.GOOS == "windows" {
 		location = strings.ReplaceAll(location, "\\", "/")
 	}
-
 	if index := strings.Index(location, "://"); index != -1 {
 		location = string(location[index+3:])
 		index := strings.Index(location, "/")
