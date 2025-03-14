14198c73848230f1dae85b5d5502d8205f75a46a
David Choi <dchoi@viantinc.com>
2025-03-13 17:20:22 -0700

    Add failing test for stated case.

--- zip/manager_test.go
+++ zip/manager_test.go
@@ -3,23 +3,29 @@ package zip_test
 import (
 	"context"
 	"fmt"
+	"path"
+	"runtime"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/viant/afs"
 	"github.com/viant/afs/option"
 	"github.com/viant/afs/storage"
-	"path"
-	"runtime"
-	"testing"
 )
 
 type useCaseFn func(s afs.Service, ctx context.Context, url string) ([]storage.Object, error)
 
 func TestNew(t *testing.T) {
 	testCases(t, func(service afs.Service, ctx context.Context, url string) ([]storage.Object, error) {
-			return service.List(ctx, url)
+		return service.List(ctx, url)
 	})
 }
 
+func TestNoCache(t *testing.T) {
+	testCases(t, func(service afs.Service, ctx context.Context, url string) ([]storage.Object, error) {
+		return service.List(ctx, url, &option.NoCache{Source: option.NoCacheBaseURL})
+	})
+}
 
 func testCases(t *testing.T, callList useCaseFn) {
 	_, filename, _, _ := runtime.Caller(0)
@@ -62,8 +68,24 @@ func testCases(t *testing.T, callList useCaseFn) {
 	}
 }
 
-func TestNoCache(t *testing.T) {
-	testCases(t, func(service afs.Service, ctx context.Context, url string) ([]storage.Object, error) {
-			return service.List(ctx, url, &option.NoCache{Source: option.NoCacheBaseURL})
-	})
+func TestCopy(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	baseDir, _ := path.Split(filename)
+	ctx := context.Background()
+
+	service := afs.New()
+
+	srcURL := fmt.Sprintf("file:%v/test/nosubdir.zip/zip://localhost", baseDir)
+	destURL := "/tmp/nosubdir"
+
+	err := service.Copy(ctx, srcURL, destURL)
+	assert.Nil(t, err)
+
+	zipObjects, err := service.List(ctx, srcURL)
+	assert.Nil(t, err)
+	assert.EqualValues(t, 2, len(zipObjects), "expected 2 objects in zip")
+
+	objects, err := service.List(ctx, destURL)
+	assert.Nil(t, err)
+	assert.EqualValues(t, 2, len(objects), "expected 2 objects in dest")
 }
